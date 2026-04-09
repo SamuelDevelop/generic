4c8aa171a6860d25af4e41a29fcecec31f9f95be
SamuelDevelop <ssoaresdossantos088@gmail.com>
2026-04-09 09:54:05 -0300

    Inicio Página de Registro

---
+++ code/frontend/app/register/page.tsx
@@ -0,0 +1,12 @@
+
+function RegisterPage(){
+    return(
+        <>
+            <main className="allScreen">
+                <h1>Tornar-se Generic</h1>
+            </main>
+        </>
+    )
+}
+
+export default RegisterPage;
