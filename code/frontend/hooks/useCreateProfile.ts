d9a7e8645939fa412cfc0f959b016ccadd9fde2a
SamuelDevelop <ssoaresdossantos088@gmail.com>
2026-04-30 13:27:11 -0300

    create(front): hook useCreateProfile

---
+++ code/frontend/hooks/useCreateProfile.ts
@@ -0,0 +1,11 @@
+'use client'
+
+import { useState } from "react";
+
+export function useCreateProfile(){
+    const [nickName, setNickName] = useState<string>("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const today = new Date().toISOString().slice(0, 10);
+    const [date, setDate] = useState(today);
+}
